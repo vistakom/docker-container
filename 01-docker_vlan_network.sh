10e42fc7dafd7871f40ba9fa4400517ad00bd16e
vistakom <152764051+vistakom@users.noreply.github.com>
2023-12-13 06:52:07 +0700

    Update 01-docker_vlan_network.sh

--- 01-docker_vlan_network.sh
+++ 01-docker_vlan_network.sh
@@ -1,1 +1,8 @@
-
+# the puspose of this script is to create vlan network inside docker that can use by container to connect with other container. With this vlan, docker container can running with dedicated ip address instead bind host ip address
+set -x
+docker network create -d ipvlan \
+    --subnet=192.168.43.0/24 \
+    --gateway=192.168.43.2 \
+    -o ipvlan_mode=l2 \
+    -o parent=eth0 mylab_net
+set +x
